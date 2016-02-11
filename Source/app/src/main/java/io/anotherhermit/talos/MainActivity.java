26b861a2b8ec3f730fa9d7c6689cfe99a10262e1
Conrad Wahlén <conrad.wahlen@gmail.com>
2016-02-11 14:51:29 +0100

    Some things needed to change

--- Source/app/src/main/java/io/anotherhermit/talos/MainActivity.java
+++ Source/app/src/main/java/io/anotherhermit/talos/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_bar_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
